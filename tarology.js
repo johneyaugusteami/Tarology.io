bf688eeda83ef09bf76cf924453395b362901d43
johneyaugusteami <jrmartinez1@my.waketech.edu>
2022-10-04 15:34:28 -0400

    fixed plaement of js file

---
+++ tarology.js
@@ -0,0 +1,17 @@
+var card1 = document.getElementById("card1"),
+    card2 = document.getElementById("card2"),
+    card3 = document.getElementById("card3"),
+    card4 = document.getElementById("card4");
+
+
+
+var intuitivetarot = [
+    {
+        card: "The Fool 0",
+        meaning: "",
+        img: ""
+    },
+
+
+
+]
